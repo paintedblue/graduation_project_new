083d8ec724000d727bc59e6f58dc7dce54353eb0
An-JiYun <81212388+An-JiYun@users.noreply.github.com>
2024-05-24 10:15:29 +0900

    feat.선호하는것저장

--- backend/src/api/routes/preferencesRoutes.js
+++ backend/src/api/routes/preferencesRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const preferencesController = require('../controllers/preferencesController');
 
-router.get('/', preferencesController.getPreferences);
-router.post('/', preferencesController.savePreferences);
+router.post('/', preferencesController.savePreferences);  // POST 엔드포인트
+router.get('/:userId', preferencesController.getPreferences);  // GET 엔드포인트 추가
 
 module.exports = router;
