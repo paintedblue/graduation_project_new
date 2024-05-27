e99c7ee0fa6eb5d1727fc44c8475ecf11d65f09a
An-JiYun <81212388+An-JiYun@users.noreply.github.com>
2024-05-27 21:30:49 +0900

    feat.선호도 저장 완

--- backend/src/api/controllers/preferencesController.js
+++ backend/src/api/controllers/preferencesController.js
@@ -12,8 +12,9 @@ mongoose.connect("mongodb://localhost:27017/grad_pro").then(() => {
 
 // 사용자 선호 데이터를 저장하는 컨트롤러
 exports.savePreferences = async (req, res) => {
+  console.log("선호도 fetch 받음");
+  const { userId, field, value, count } = req.body;
   try {
-    const { userId, field, value } = req.body;
     //const characterToSave = value.slice(0, 1);  // 문자열에서 첫 글자를 추출
 
     const prompt = createExtractPrompt(value);
@@ -28,7 +29,7 @@ exports.savePreferences = async (req, res) => {
     preferences[field] = gptResponse;
     await preferences.save();
 
-    res.status(201).json({ value: preferences[field] });
+    res.status(201).json( preferences[field] );
   } catch (error) {
     console.error('Error saving user preferences:', error);
     res.status(500).json({ message: 'Error saving user preferences', error });
